6f327fc3cea22a049364c571005f3a9c39bb3abf
usman ali <usmanali4073@gmail.com>
2020-03-05 21:05:35 -0500

    state

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -3,7 +3,10 @@ import React, { Component }  from 'react';
 class Account extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            name:'usman ali',
+            age: 40
+          }
     }
     render() { 
         return (
@@ -11,6 +14,8 @@ class Account extends Component {
             <button onClick={(c)=>{console.log("A")}}>A</button>
             <button onClick={(c)=>{console.log("B")}}>B</button>
             <p>{Math.random() * 10}</p>
+
+            <p> {this.state.name} is going to be {this.state.age}</p>
         </div> 
           );
     }
